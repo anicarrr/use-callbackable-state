0690a2f77e3dd5a30c5565cfe218581d20c6984b
Anicar Cabrera <anicarrr@gmail.com>
2020-03-22 21:39:23 -0300

    add example and README

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import { useCallbackableState } from 'use-callbackable-state';
 
-import ExampleComponent from 'use-callbackable-state'
+function App() {
+  const [isLoading, setLoading] = useCallbackableState(false);
+  const [data, setData] = useState([]);
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
+  function handleButtonClick() {
+    setLoading(true, () => {
+      // simulate 1 second fetching
+      setTimeout(() => {
+        setData([
+          { description: "Item 1"},
+          { description: "Item 2"},
+        ]);
+        setLoading(false);
+      }, 1000);
+    });
   }
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>Fetch from API</button>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && data.length > 0 && <p>{JSON.stringify(data, null, 1)}</p>}
+    </div>
+  )
 }
+
+export default App;
